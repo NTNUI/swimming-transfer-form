d20ddf8613f40a70da08d2fd33f929d2c62288b5
Pavel Skipenes <pavelgs@stud.ntnu.no>
2022-04-07 20:42:30 +0200

    give this commit a good title
    - update transfer forms to 2022 from 2020 versions
    - update example.csv to use updated columns from db
    - add requirements.txt
    - update README.md
    - generate both forms
    - gitignore all .csv files
    - don't generate coordinates dynamically
    - update directory structure

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # Swimming transfer form generator
 
-auto generate transfer forms ("overgangsskjema") for new members
+Auto generate transfer forms ("Overgangsskjema") and association transfer forms ("Endring av klubbtilhørighet") for new members.
 
-# Prerequisites
-TODO: add required packages
+Whenever new athletes tries to join a new swimming group they will need to transfer their club association. This is necessary because only one club at a time can generate payment information for the swimming license. And without the license the athlete might not compete in official events or attend practices. Unfortunately, this process cannot be fully automated because signature of the athlete is required. However board member responsible for filling out the form will have much less work.
 
-# Usage
+For some unknown reason there are two forms. If the athlete have had an active membership the past two years then "Overgangsskjema" needs to be used. Otherwise the "Endring av klubbtilhørighet" has to be used. The form needs to be sent to the athlete after it has been generated. The athlete will then have to sign the form and send it to Norwegian Swimming Federation by email.
+
+
+## Prerequisites
+- python3
+
+```
+pip install -r requirements.txt
+```
+
+
+## Example
+```
+python transfer-generator.py assets/example.csv
+```
+
+
+## Usage
 1. Login into phpmyadmin [https://mysqladmin.it.ntnu.no/](https://mysqladmin.it.ntnu.no/)
 2. open members table
 3. click on export and use following settings:
@@ -21,9 +37,7 @@ Put columns names in the first row: true
 python transfer-generator.py member.csv 
 ```
 
-# Download updated zip codes
-[https://www.bring.no/tjenester/adressetjenester/postnummer](https://www.bring.no/tjenester/adressetjenester/postnummer)
+### Other
 
-
-# Download new transfer forms
-[https://svomming.no/forbundet/klubbdrift/organisasjon/overganger/](https://svomming.no/forbundet/klubbdrift/organisasjon/overganger/)
+[Source Norwegian zip codes](https://www.bring.no/tjenester/adressetjenester/postnummer)
+[Source transfer forms](https://svomming.no/forbundet/klubbdrift/organisasjon/overganger/)
--- transfer-generator.py
+++ transfer-generator.py
@@ -1,68 +1,60 @@
-#! /usr/bin/python
-import sys
-import os
+#!/usr/bin/python
+
+from datetime import datetime
 from PyPDF3 import PdfFileWriter, PdfFileReader
-import io
-from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.utils import ImageReader
-from reportlab.platypus import Image
+from reportlab.pdfgen import canvas
 import csv
-from datetime import datetime
+import io
+import os
+import sys
 
-zip_codes_file = "zip-codes.txt"
-transfer_form_file = "overgangsskjema2020.pdf"
-association_transfer_file = "EndringAvKlubbtilhorighet2020.pdf"
-signature_file = "signature.png"
-signature_height = 20
+zip_codes_file = "assets/zip-codes.txt"
+transfer_form_file = "assets/overgangsskjema2022.pdf"
+association_transfer_file = "assets/EndringAvKlubbtilhorighet2022.pdf"
+signature_file = "assets/signature.png"
 
-# signature coordinates
-transfer_signature_x = 142
-transfer_signature_y = 670
-association_signature_x = 142
-association_signature_y = 589
+coords_transfer_pdf = {
+    'name': (146, 624),
+    'birth_date': (146, 596),
+    'address': (146, 568),
+    'zip': (146, 540),
+    'location': (262, 540),
+    'licensee': (146, 512),
+    'new': (146, 484),
+    'X': (170, 445),
+    'date': (186, 386),
+    'sign': (186, 346)
+}
 
-
-def get_positions(x1_, y1_):
-    # First group positions (Navn -> Ny klubb)
-    x1 = x1_
-    xp = x1 + 116  # zip position
-    y1 = y1_
-    dy1 = 28  # Distance between fields in first group
-
-    # Checkmark position (gren)
-    xc = x1+28
-    yc = y1 - dy1*6 - 7
-
-    # Second group positions (Sign)
-    x2 = x1 + 40
-    y2 = y1 - 228
-    dy2 = 40
-
-    return {
-        "name": (x1, y1 - dy1*0),
-        "birth_date": (x1, y1 - dy1*1),
-        "address": (x1, y1 - dy1*2),
-        "zip": (x1, y1 - dy1*3),
-        "location": (xp, y1 - dy1*3),
-        "licensee": (x1, y1 - dy1*4),
-        "new": (x1, y1 - dy1*5),
-        "X": (xc, yc),
-        "date": (x2, y2 - dy2*0),
-        "sign": (x2, y2 - dy2*1),
-    }
+coords_association_pdf = {
+    "name": (146, 569),
+    "birth_date": (146, 541),
+    "address": (146, 513),
+    "zip": (146, 485),
+    "location": (262, 485),
+    "licensee": (146, 457),
+    "new": (146, 429),
+    "X": (170, 394),
+    "date": (186, 341),
+    "sign": (186, 301),
+}
 
 
 def write_to_pdf(input_data, output_positions, templatefile, outputfile, signature=None):
     packet = io.BytesIO()
     # create a new PDF with Reportlab
     can = canvas.Canvas(packet, pagesize=A4)
+    # insert text into pdf
     for entry in input_data:
         can.drawString(
             output_positions[entry][0], output_positions[entry][1], input_data[entry])
+    # insert signature into pdf
     if signature != None:
         iw, ih = signature.getSize()
         aspect = iw / float(ih)
+        signature_height = 20
         can.drawImage(signature, output_positions['sign'][0], output_positions['sign'][1],
                       width=(signature_height * aspect), height=signature_height)
     can.save()
@@ -81,48 +73,57 @@ def write_to_pdf(input_data, output_positions, templatefile, outputfile, signatu
     output.write(outputStream)
     outputStream.close()
 
-# use signature file
-signature = None
-if os.path.isfile(signature_file):
-    signature = ImageReader(signature_file)
 
 # read zip number and location from file
-zip_locations = {}
-with open(zip_codes_file, newline='', encoding="utf-8") as csvfile:
-    reader = csv.reader(csvfile, delimiter='\t')
-    for row in reader:
-        zip_locations[int(row[0])] = row[1]
+def get_zip_locations():
+    zip_locations = {}
+    with open(zip_codes_file, newline='', encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        for row in reader:
+            zip_locations[int(row[0])] = row[1]
+    return zip_locations
 
-with open(sys.argv[1], newline='', encoding='utf8') as csvfile:
-    output_positions = {}
-    input_file = ""
-    reader = csv.DictReader(csvfile, delimiter=',')
-    for row in reader:
-        data = {
-            'name': row['first_name'] + " " + row['surname'],
-            'birth_date': row['birth_date'],
-            'address': row['address'],
-            'licensee': row['licensee'],
-            'new': "NTNUI-Svømming",
-            'X': "X",
-            'date': datetime.today().strftime('%d.%m.%y') + " - Trondheim",
-        }
-        try:
-            data["zip"] = "{:04d}".format(int(row['zip']))
-            data["location"] = zip_locations[int(row['zip'])]
-        except KeyError:
-            data["zip"] = "7030"
-            data["location"] = "Trondheim"
-        if (True): # set false for association transfer
-            output_positions = get_positions(
-                transfer_signature_x, transfer_signature_y)
-            input_file = transfer_form_file
-        else:
-            output_positions = get_positions(
-                association_signature_x, association_signature_y)
-            input_file = association_transfer_file
-        if not os.path.exists("output"):
-            os.mkdir("output")
-        write_to_pdf(data, output_positions, input_file,
-                     "output/" + data['name'] + ".pdf", signature)
-        print("Done " + data["name"])
+def main():
+    with open(sys.argv[1], newline='', encoding='utf8') as csvfile:
+
+        if not os.path.exists("output/transfer"):
+            os.mkdir("output/transfer")
+        if not os.path.exists("output/association"):
+            os.mkdir("output/association")
+        
+        signature = None
+        if os.path.isfile(signature_file):
+            signature = ImageReader(signature_file)
+
+        reader = csv.DictReader(csvfile, delimiter=',')
+        for row in reader:
+            data = {
+                'name': row['first_name'] + " " + row['surname'],
+                'birth_date': row['birth_date'],
+                'address': row['address'],
+                'licensee': row['licensee'],
+                'new': "NTNUI-Svømming",
+                'X': "X",
+                'date': datetime.today().strftime('%d.%m.%y') + " - Trondheim",
+            }
+            try:
+                data["zip"] = "{:04d}".format(int(row['zip']))
+                data["location"] = get_zip_locations()[int(row['zip'])]
+            except KeyError:
+                # Some internationals use their international zip location.
+                # which will fail in the block above
+                # Use some random Trondheim location as fallback
+                data["zip"] = "7030"
+                data["location"] = "Trondheim"
+
+
+            write_to_pdf(data, coords_transfer_pdf, transfer_form_file,
+                        "output/transfer/" + data['name'] + ".pdf", signature)
+            write_to_pdf(data, coords_association_pdf, association_transfer_file,
+                        "output/association/" + data['name'] + ".pdf", signature)
+            print("Done ✅ " + data["name"])
+
+
+if __name__ == "__main__":
+    main()
+
